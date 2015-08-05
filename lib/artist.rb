cfa994a44b650f04c837d14d8bb2de5e2b12caaa
ehawk823 <evan.alexander.hawk@gmail.com>
2015-08-05 15:10:31 -0500

    done

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,3 +1,11 @@
+require_relative "./song.rb"
+
 class Artist
-  # code goes here
+
+  attr_accessor :name
+
+  def initialize(name)
+    @name = name
+  end
+
 end
--- lib/song.rb
+++ lib/song.rb
@@ -1,3 +1,18 @@
+require_relative "./artist.rb"
+require 'tempfile'
+
 class Song
-  # code goes here
+
+attr_accessor :title, :artist
+
+    def initialize
+    end
+
+    def serialize
+      clean = @title.downcase.gsub(/\s+/,"_")
+      file = Tempfile.new(["#{clean}", '.txt'], 'tmp')
+      file.write("#{@artist.name} - #{@title}")
+      file.flush
+    end
+
 end
